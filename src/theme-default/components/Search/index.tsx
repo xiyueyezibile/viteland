de63b2fd5ddd9f3d1f08066ba9fbe7da61f31c78
xiyueyezibile <2859439384@qq.com>
2025-02-17 02:07:40 +0800

    feat: add search view

--- src/theme-default/components/Search/index.tsx
+++ src/theme-default/components/Search/index.tsx
@@ -3,6 +3,9 @@ import styles from './index.module.scss';
 import { useI18n } from '@/theme-default/hooks/useI18n';
 import Flexsearch from 'flexsearch';
 import { PageData } from '@/node/types';
+
+type SearchResult = PageData & {searchIndex: number }
+
 const index = new Flexsearch.Document({
   document: {
     id: 'id',
@@ -15,6 +18,7 @@ export const Search = () => {
   const [searchKey, setSearchKey] = useState('');
   const lang = useI18n();
   const searchData = useRef<Map<number, PageData>>(null);
+  const [searchResult, setSearchResult] = useState<SearchResult[]>([]);
 
   function closePopup() {
     setShow(false);
@@ -36,16 +40,70 @@ export const Search = () => {
       console.log(error);
     }
   }
-  function search() {
+  function search(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    e.stopPropagation();
+    setShow(true);
     console.log(searchKey);
     const results = index.search(searchKey);
-    console.log(results);
+    const searchContents: SearchResult[] = []
+    results.forEach((result) => {
+        result.result.forEach((id) => {
+          const item = searchData.current?.get(id as number);
+          if (item) {
+            let content = item.content;
+            // 不区分大小写的查找
+            let i = item.content.toLowerCase().indexOf(searchKey.toLowerCase());
+            if(item.content.length > 40) {
+                content = content.slice(i-20 < 0? 0: i-20, i+20);
+            }
+            searchContents.push({...item, content: content, searchIndex: i});
+          }
+        })
+    })
+    console.log(searchContents);
+    
+    setSearchResult(searchContents);
   }
   useEffect(() => {
     if (show && !searchData.current) {
       getPageSearch();
     }
+    if (show) {
+      window.addEventListener('click', closePopup);
+    }
   }, [show]);
+
+  const SearchList = (props: {searchResult: SearchResult[]}) => {
+    const { searchResult } = props;
+        // 超出滚动
+    return <div className='max-h-[250px]' overflow="auto">
+    {searchResult.length ? searchResult.map((item, i) => {
+        const pre = item.content.slice(0, item.searchIndex);
+        const key = item.content.slice(item.searchIndex, item.searchIndex + searchKey.length);
+        const next = item.content.slice(item.searchIndex + searchKey.length);
+        return <>
+        <div cursor="pointer" onClick={() => {
+            // item.routePath 格式为 /zh-CN/docs/xxx/
+            // 想要前面不动的通过window跳转到 /zh-CN/docs/xxx
+            // item.tocTitle 格式为 xxx-1
+            let toc = item.tocTitle.length? `#${item.tocTitle}` : '';
+            if(toc) {
+                const arr = toc.split('-');
+                arr.pop()
+                toc = arr.join('-');
+            }
+            window.location.href = `${item.routePath.endsWith('/')? item.routePath.slice(0, -1) : item.routePath}${toc}`;
+          setShow(false);
+        }} p="5px" ><div text="sm" color='black dark:white'>
+        <span>{pre}</span><span color="brand">{key}</span><span>{next}</span></div>
+            <div text="sm" color='gray dark:white'>
+            {item.routePath}
+                </div></div>
+        {i !== (searchResult.length - 1) && <div className=" h-[1px] bg-gray dark:bg-white"></div>}
+        </>
+      }): <div text="sm" p="5px">没有搜索结果</div>}
+    </div>
+  }
 
   return (
     <div flex="~" ml="30px" className="relative">
@@ -54,20 +112,21 @@ export const Search = () => {
           onClick={(e) => {
             e.stopPropagation();
           }}
-          className="absolute top-[35px] w-full bg-white"
+          className="absolute top-[35px] w-[200px] bg-white"
           p="5px "
           rounded="5px"
           border="0.5px solid black dark:white">
-          123
+          <SearchList searchResult={searchResult} />
         </div>
       )}
       <input
         onClick={(e) => {
           e.stopPropagation();
-          setShow(true);
-          window.addEventListener('click', closePopup);
+            setShow(true);
         }}
-        onChange={(e) => setSearchKey(e.target.value)}
+        onChange={(e) => {
+            setSearchKey(e.target.value)
+        }}
         type="text"
         border="0.5px solid black dark:white"
         mr="5px"
